fbea3b474768709d8551aaf8d9b54865b3c130e4
hagit8 <hagit456@gmail.com>
2026-03-15 10:35:32 +0200

    Add Terraform configuration for Nginx Docker container
    This Terraform configuration sets up a Docker provider, pulls the latest Nginx image, and creates a container with port mapping.

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pulls the image
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+# Creates a container
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial-container"
+  
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
